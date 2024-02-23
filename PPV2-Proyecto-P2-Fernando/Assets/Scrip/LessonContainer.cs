c047dc3030f8ecf15f649df2be0df0ddc95c4fd8
cacaguaton <fernando.martinez@uteca.edu.mx>
2024-02-23 17:53:23 -0600

    main- Create  LessonContainer Scrip
    Creamos el scrip y asociamos los botones necesarios

---
+++ PPV2-Proyecto-P2-Fernando/Assets/Scrip/LessonContainer.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class LessonContainer : MonoBehaviour
+{
+    [Header("GameObject Configuration")]
+    public int Lection = 0;
+    public int CurrentLession = 0;
+    public int TotalLessions = 0;
+    public bool AreAllLessionsComplete = false;
+
+    [Header("UI Configuration")]
+    public TMP_Text StageTitle;
+    public TMP_Text LessonStage;
+
+    [Header("External GameObject Configuration")]
+    public GameObject lessonContainer;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lessonContainer != null)
+        {
+            OnUptateUI();
+        }
+        else
+        {
+            Debug.LogWarning("GameObject Nulo, revisa las variables de tipo GameObject lessonContainer");
+        }
+    }
+
+    public void OnUptateUI()
+    {
+        if (StageTitle != null || LessonStage != null)
+        {
+            StageTitle.text = "Leccion " + Lection;
+            LessonStage.text = "Leccion " + CurrentLession + " de " + TotalLessions;
+        }
+        else
+        {
+            Debug.LogWarning("GameObject Nulo, revisa las variables de tipo TMP_Text");
+        }
+    }
+
+    //Este metodo activa/desactiva la ventana de lessonContainer
+    public void EnableWindows()
+    {
+        OnUptateUI();
+
+        if(lessonContainer.activeSelf)
+        {
+            // Desactiva el objeto si está activo
+            lessonContainer.SetActive(false);
+        }
+        else
+        {
+            // Activa el objeto si está desactivado
+            lessonContainer.SetActive(true);
+        }
+    }
+
+}
