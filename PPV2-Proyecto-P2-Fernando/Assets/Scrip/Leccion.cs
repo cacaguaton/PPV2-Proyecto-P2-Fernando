4a924b82c8c4b4f29892c569e1965497464fa2b9
FernandoMartinez <157182702+cacaguaton@users.noreply.github.com>
2024-03-15 13:44:46 -0600

    Update Leccion.cs
    Agregue comentarios

--- PPV2-Proyecto-P2-Fernando/Assets/Scrip/Leccion.cs
+++ PPV2-Proyecto-P2-Fernando/Assets/Scrip/Leccion.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//creamos una clase para almacenar tener de referencia los datos
+//que vamosa utilizar 
+//usamos serializable para que se va en el insupector
 [System.Serializable]
 public class Leccion
 {
+    //almacenamos el numero de pregunta
     public int ID;
+    //almacenamis la pregunta
     public string lessons;
+    //almacenamis las opciones de respuesta
     public List<string> options;
+    //almacenamos la respuesta correcta 
     public int correctAnswer;
 }
