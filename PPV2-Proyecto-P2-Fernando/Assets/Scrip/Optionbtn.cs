6e3a7fbcb4e41a8e03a64219e36953c0386a3ed2
FernandoMartinez <157182702+cacaguaton@users.noreply.github.com>
2024-03-15 14:06:21 -0600

    Update Optionbtn.cs

--- PPV2-Proyecto-P2-Fernando/Assets/Scrip/Optionbtn.cs
+++ PPV2-Proyecto-P2-Fernando/Assets/Scrip/Optionbtn.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+//scrip para los botones
 public class Optionbtn : MonoBehaviour
 {
+//numero de boton
     public int OptionID;
+    //guardamos el nombre de la respuesta por boton
     public string OptionName;
     // Start is called before the first frame update
     void Start()
     {
+    //el boton tiene de hijo un texto, lo busca y le sustituye el texto por la respuesta
+    //que va en ese boton
         transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
     }
 
     public void UptateText()
     {
+    //cuando se cambia de pregunta se actualiza 
+    //el nombre de cada opcion
         transform.GetChild(0).GetComponent<TMP_Text>().text = OptionName;
     }
 
