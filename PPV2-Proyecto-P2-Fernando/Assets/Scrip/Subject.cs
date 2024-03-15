ea3da94aef039e74d035546416f6a4beae07ff82
FernandoMartinez <157182702+cacaguaton@users.noreply.github.com>
2024-03-15 14:00:44 -0600

    Update Subject.cs
    Agregue comentarios

--- PPV2-Proyecto-P2-Fernando/Assets/Scrip/Subject.cs
+++ PPV2-Proyecto-P2-Fernando/Assets/Scrip/Subject.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//creamos la base del scriptableobject
 [CreateAssetMenu(fileName = "New Subject", menuName = "ScriptableObjects/New_Lesson", order = 1)]
 public class Subject : ScriptableObject
 {
+//asignamos numero de leccion
     [Header("GameObject Configuration")]
     public int Lesson = 0;
+    //le damos la estructura que le pusimos en la leccion y creamos una lista de esta
     [Header("Lession Quest Configuration")]
     public List<Leccion> leccionList;
 
